0dd0f1c7d650c3a41b15267bd15de51c56ec9649
QbinVA <valdokevin02@gmail.com>
2025-03-11 09:59:02 -0600

    base de datos y correciones con el server localhost

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+// db.php
+
+$host = 'localhost';
+$dbname = 'poder_igualitario';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+    exit;
+}
+?>
